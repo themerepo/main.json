ceb57d31f47e2cfc4a1db64465972aa75a1b953d
Zach <zbecker10@gmail.com>
2016-01-25 17:27:15 -0500

    HERRO

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,36 @@
-var app = angular.module('ThemeRepoToolMainJSON', []);
+var app = angular.module('ThemeRepoToolMainJSON', [], function($interpolateProvider) {
+  $interpolateProvider.startSymbol('[[');
+  $interpolateProvider.endSymbol(']]');
+});
+app.controller("ThemeRepoToolMainJSONMain", function($scope) {
+    $scope.theme = {};
+    $scope.dump = function() {
+        console.log($scope.theme);
+    }
+});
+app.directive('ngCleanInput', function() {
+    return {
+        link: function(scope, element, attrs) {
+            attrs.$set('autocomplete', "off");
+            attrs.$set('autocorrect', "off");
+            attrs.$set('autocapitalize', "off");
+            attrs.$set('spellcheck', "false");
+        }
+    }
+});
+app.directive('ngAutoExpand', function() {
+    return {
+        link: function(scope, element, attrs) {
+            var textarea = element[0];
+            textarea.oninput = function() {
+                textarea.style.height = "62px"; /* Reset the height*/
+                textarea.style.height = Math.min(textarea.scrollHeight, 300) + "px";
+            };
+        }
+    }
+});
+app.filter('prettify', function () {
+    return function (json) {
+        return angular.toJson(json, 4);
+    };
+});
